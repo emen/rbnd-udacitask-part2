5b07b814bfdb5160533f05a45f53afc4548788f1
Emen Zhao <emenzhaogq@gmail.com>
2016-04-22 23:24:34 -0700

    Refactor and move format methods to its own module

--- lib/listable.rb
+++ lib/listable.rb
@@ -1,3 +1,30 @@
 module Listable
-  # Listable methods go here
+
+  def format_description(description)
+    "#{description}".ljust(25)
+  end
+
+  def format_date_range(start_date = nil, end_date = nil)
+    dates = start_date.strftime("%D") if start_date
+    dates << " -- " + end_date.strftime("%D") if end_date
+    dates = "N/A" if !dates
+    return dates
+  end
+
+  def format_name(site_name = nil)
+    site_name ? site_name : ""
+  end
+
+  def format_due_date(due = nil)
+    due ? due.strftime("%D") : "No due date"
+  end
+
+  def format_priority(priority = nil)
+    value = " ⇧" if priority == "high"
+    value = " ⇨" if priority == "medium"
+    value = " ⇩" if priority == "low"
+    value = "" if !priority
+    return value
+  end
+
 end
--- lib/todo.rb
+++ lib/todo.rb
@@ -1,5 +1,5 @@
 class TodoItem
-  # include Listable
+  include Listable
   attr_reader :description, :due, :priority
 
   PRIORITIES = %w(high medium low)
@@ -12,22 +12,9 @@ class TodoItem
       raise UdaciListErrors::InvalidPriorityValue
     end
   end
-  def format_description
-    "#{@description}".ljust(25)
-  end
-  def format_date
-    @due ? @due.strftime("%D") : "No due date"
-  end
-  def format_priority
-    value = " ⇧" if @priority == "high"
-    value = " ⇨" if @priority == "medium"
-    value = " ⇩" if @priority == "low"
-    value = "" if !@priority
-    return value
-  end
   def details
-    format_description + "due: " +
-    format_date +
-    format_priority
+    format_description(description) + "due: " +
+    format_due_date(due) +
+    format_priority(priority)
   end
 end
